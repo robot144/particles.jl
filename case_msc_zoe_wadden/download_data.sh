e531207ee03afea22d41359c702d35212e4379b4
robot144 <martin@verlaan.info>
2019-11-07 09:34:57 +0100

    Add MSC Zoe case data.

---
+++ case_msc_zoe_wadden/download_data.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+# This script downloads the relevant data for this case. This data is not inside the repository 
+# because of its size.
+
+# 165Mb
+curl -o era5_wind_201403_05.nc https://nx7384.your-storageshare.de/s/HosJSLErsgLd4TT/download
