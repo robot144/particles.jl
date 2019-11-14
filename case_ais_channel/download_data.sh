5b49e37cb59553ce969bfd8de6da3666f4622124
robot144 <verlaanm@gmail.com>
2019-11-14 17:25:33 +0100

    add gtsm data for ais channel case

---
+++ case_ais_channel/download_data.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+# This script downloads the relevant data for this case. This data is not inside the repository 
+# because of its size.
+
+# GTSM-FM curents 2D
+# 1.6 Gb 
+curl -o gtsm_fine_0002_map.nc https://nx7384.your-storageshare.de/s/Qtw2jrc9PP243a8/download 
--- case_ais_channel/plot_ais.jl
+++ case_ais_channel/plot_ais.jl
@@ -27,6 +27,28 @@ for si=1:5
    push!(shipnames,t[si].Name[20])
 end
 
+# read GTSM current data
+dflow_map=load_nc_info(".",r"gtsm_fine_...._map.nc")
+t0=get_reftime(dflow_map)
+# create interpolation functions u1(x,y,z,t) and v1(x,y,z,t) with t in seconds relative to t0
+u1,v1=initialize_interpolation(dflow_map,interp,t0)
+# some tests
+#interp=load_dflow_grid(dflow_map,50,false)
+#ind=find_index(interp,-2.0,50.0)
+#ff=u1(-2.0,50.0,0.0,0.) #lon,lat,dummy,t with t in seconds relative to t0
+# create timeseries at one point
+lon=-2.0
+lat=50.0
+ts=(14.0*24.0*3600.0):1800.0:(18.0*24.0*3600.0) #times in seconds since t0
+tt=t0+ts.*Second(1) #convert to DateTime
+uu=zeros(length(tt))
+vv=zeros(length(tt))
+for ti=1:length(tt)
+  uu[ti]=u1(lon,lat,0.0,ts[ti])
+  vv[ti]=v1(lon,lat,0.0,ts[ti])
+end
+plot(tt,[uu,vv],label=["u east","v north"])
+
 #
 # plot traks on a background
 #
