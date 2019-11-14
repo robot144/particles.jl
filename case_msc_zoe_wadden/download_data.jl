8081645fdadb7b93430146ce02fe1120c7fd77e0
robot144 <verlaanm@gmail.com>
2019-11-14 19:30:35 +0100

    Fix download scripts for zoe case

---
+++ case_msc_zoe_wadden/download_data.jl
@@ -0,0 +1,30 @@
+# This script downloads the relevant data for this case. This data is not inside the repository 
+# because of its size.
+
+using HTTP
+# GTSM-FM curents 2D
+HTTP.download("https://nx7384.your-storageshare.de/s/Qtw2jrc9PP243a8/download","gtsm_fine_0002_map.nc")
+
+
+# DCSM-FM curents 2D
+# 20*2Gb 
+HTTP.download("https://nx7384.your-storageshare.de/s/CHzQRLJiHPGGfsX/download","DCSM-FM_0_5nm_0000_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/BxMcAZ6n3yqSMsR/download","DCSM-FM_0_5nm_0001_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/N3Zf49jR6a95rSx/download","DCSM-FM_0_5nm_0002_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/PSpoMDSs7Zc2LLZ/download","DCSM-FM_0_5nm_0003_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/epPpFLJHK3Aikz9/download","DCSM-FM_0_5nm_0004_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/Zf8sxa7XJDQNsi9/download","DCSM-FM_0_5nm_0005_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/jxWE5es258Ytk8Z/download","DCSM-FM_0_5nm_0006_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/rsojKZBcGte9Eb6/download","DCSM-FM_0_5nm_0007_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/J5x2W2jf7pk6SZw/download","DCSM-FM_0_5nm_0008_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/LWrazPqjpx3FcgT/download","DCSM-FM_0_5nm_0009_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/Z2t4dTwPK44CrxM/download","DCSM-FM_0_5nm_0010_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/cH58jsagaGimzbg/download","DCSM-FM_0_5nm_0011_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/mxiA8D6YA9mBEAg/download","DCSM-FM_0_5nm_0012_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/aKDzrNE97XBMTHY/download","DCSM-FM_0_5nm_0013_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/N5fRT6jYQFYj4Lo/download","DCSM-FM_0_5nm_0014_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/4CsgmrjEjdPoP8q/download","DCSM-FM_0_5nm_0015_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/zijpCYz5Bk3cRcx/download","DCSM-FM_0_5nm_0016_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/9mwaeoj3QDjAy9D/download","DCSM-FM_0_5nm_0017_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/fn2LMSPmatLXBpG/download","DCSM-FM_0_5nm_0018_map.nc")
+HTTP.download("https://nx7384.your-storageshare.de/s/TfdAGSppJS7JZzt/download","DCSM-FM_0_5nm_0019_map.nc")
