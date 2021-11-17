ac963e9fb1f090b4380ab21ce0459c6ff14979b8
Robot 144 <robot144@protonmail.com>
2021-11-17 09:28:32 +0100

    Update download_data.jl
    Also extend to 31 days of wind for hindcast

--- case_drifter_challenge/data/hindcast/download_data.jl
+++ case_drifter_challenge/data/hindcast/download_data.jl
@@ -9,5 +9,7 @@ HTTP.download("https://nx7384.your-storageshare.de/s/BG3reLnBJktAMwz/download","
 HTTP.download("https://nx7384.your-storageshare.de/s/KgKsdmWk5FqbZPP/download","v0_2021-05-29_00-00-00_2021-06-22_00-00-00.nc")
 
 # winds from NOAA GFS
-# 48Mb
-HTTP.download("https://nx7384.your-storageshare.de/s/K34zDXKK2GpPLBM/download","gfs_winds.nc")
+# 48Mb one day
+#HTTP.download("https://nx7384.your-storageshare.de/s/K34zDXKK2GpPLBM/download","gfs_winds.nc")
+# 763Mb 31 days
+HTTP.download("https://nx7384.your-storageshare.de/s/jMQtERxRakj59ii/download,"gfs_winds.nc")
