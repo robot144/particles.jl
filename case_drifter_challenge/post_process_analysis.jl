59a5c321c56e4267b22330bd32914964f2f37dd7
Robot144 <robot144@protonmail.com>
2021-11-22 07:46:01 +0100

    postprocess also ndrifters>1

--- case_drifter_challenge/post_process_analysis.jl
+++ case_drifter_challenge/post_process_analysis.jl
@@ -20,7 +20,7 @@ particlefile="netcdf_output/darpa_hcast.nc"
 #runid="leeway0.0072npart1k1.0"
 #runid="leeway0.0288npart1k1.0"
 #runid="leeway0.0100npart1k1.0"
-runid="leeway0.0144npart10k1.0"
+runid="leeway0.0144npart10k500.0"
 
 # load tracks and look for starting points
 df = DataFrame(CSV.File(spotterfile))
@@ -58,6 +58,8 @@ end
 output=NetCDF.open(particlefile)
 p_lon=output.vars["lon"][:,:,1]
 p_lat=output.vars["lat"][:,:,1]
+n=size(p_lon,2) # total nummber of particles
+npart=div(n,ndrifters)
 tref=DateTime("2021-05-30T00:15:00") #TODO read from netcdf
 p_time=output.vars["time"][:]
 istart=2
@@ -69,6 +71,8 @@ iend=490
 t_meas  =p_time[istart:iend]
 ntime=length(t_meas)
 rmse=zeros(ndrifters,ntime)
+p_lon_avg=zeros(ntime,ndrifters)
+p_lat_avg=zeros(ntime,ndrifters)
 for i=1:ndrifters
    println("drifter $(i)")
    t_tmp=div.(measurements[i].time-tref,Dates.Millisecond(1000))
@@ -78,8 +82,14 @@ for i=1:ndrifters
    flat=Interpolations.LinearInterpolation(t_tmp,lat_tmp)
    lon_meas=flon.(t_meas) #index 1 outside interval for measurements
    lat_meas=flat.(t_meas)
-   lon_mod=p_lon[istart:iend,i]
-   lat_mod=p_lat[istart:iend,i]
+   imin=npart*(i-1)+1
+   imax=npart*i
+   lon_mod_all=p_lon[istart:iend,imin:imax]
+   lat_mod_all=p_lat[istart:iend,imin:imax]
+   lon_mod=sum(lon_mod_all,dims=2)/size(lon_mod_all,2)
+   lat_mod=sum(lat_mod_all,dims=2)/size(lat_mod_all,2)
+   p_lon_avg[:,i].=lon_mod[:]
+   p_lat_avg[:,i].=lat_mod[:]
    dx=1e-3*R*deg2rad.(lon_mod-lon_meas).*cosd.(lat_meas)
    dy=1e-3*R*deg2rad.(lat_mod-lat_meas)
    dist=sqrt.(dx.^2 .+ dy.^2)
@@ -121,11 +131,12 @@ ylabel!("latitude")
 savefig("measured_spotter_positions.png")
 
 plot_image(img,boundbox)
-plot!(lon,lat,seriestype = :scatter, label = "initial position")
+plot!(p_lon,p_lat,color=:lightblue,label="")
+plot!(lon,lat,seriestype = :scatter, lw=2,label = "initial position")
 for i=1:length(measurements)
    plot!(measurements[i].lon,measurements[i].lat,color=:red,label="")
 end
-plot!(p_lon,p_lat,color=:blue,label="")
+plot!(p_lon_avg,p_lat_avg,color=:blue,lw=2,label="")
 title!("Modeled and measured tracks")
 xlabel!("longitude")
 ylabel!("latitude")
