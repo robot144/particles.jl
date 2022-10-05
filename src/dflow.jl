1ebd903796c0a9fe0c0a64b86895d13f0bbb51a4
Robot144 <robot144@protonmail.com>
2022-10-05 21:14:17 +0200

    Small but with joinpath.

--- src/dflow.jl
+++ src/dflow.jl
@@ -266,7 +266,7 @@ function initialize_interpolation(dflow_map, interp::Interpolator, varname, reft
          if !isnothing(dflow_map_ind_needed)
             dflow_map_ind_needed = (dflow_map_ind_needed-no_domains+1):dflow_map_ind_needed
             if dflow_map_ind_needed != dflow_map_ind # update of dflow_map needed (e.g. using run02/run_0000_map.nc instead of run01/run_0000_map.nc)
-               (debuglevel >= 1) && println("t = $(t) - Switching to dflowfm_map based on: ..\\$(joinpath(splitpath(dflow_map_all[dflow_map_ind_needed[1]].name)[end-2:end]))")
+               (debuglevel >= 1) && println("t = $(t) - Switching to dflowfm_map based on: ../$(joinpath(splitpath(dflow_map_all[dflow_map_ind_needed[1]].name)[end-2:end]...))")
                dflow_map_ind = dflow_map_ind_needed
                dflow_map = dflow_map_all[dflow_map_ind]
                times_cache = times_cache_all[dflow_map_ind[1]]
