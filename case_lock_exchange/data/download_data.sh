f6df03453673e452346fddd515486f7e0b390eb1
robot144 <robot144@github.com>
2022-11-11 18:13:34 +0100

    Start case for lock-exchange

---
+++ case_lock_exchange/data/download_data.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# This script downloads the relevant data for this case. This data is not inside the repository 
+# because of its size.
+
+# Maps for lock exchange test case
+# 81.1Mb
+wget -O locxx_map.nc https://nx7384.your-storageshare.de/s/fe2KHoLBGQx3S46/download
+
