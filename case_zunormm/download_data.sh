d63d7992ed8cd22a9a92c1d9d5199129d82d9731
robot144 <robot144@protonmail.com>
2023-08-20 00:12:42 +0200

    Replace datafile for zunormm. Add waterdepth.

--- case_zunormm/download_data.sh
+++ case_zunormm/download_data.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 #
 # 
-curl -o zunormm.tgz  https://nx7384.your-storageshare.de/s/BbWGcMxHr4G3TpQ/download
+#curl -o zunormm.tgz  https://nx7384.your-storageshare.de/s/BbWGcMxHr4G3TpQ/download
+# new datafile with waterdepth included
+curl -o zunormm.tgz  https://nx7384.your-storageshare.de/s/5Br4YNqjCiZK3ms/download
 
 tar -xzf zunormm.tgz
