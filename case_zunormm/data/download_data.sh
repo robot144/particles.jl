6fef6def4381868457ce58ee1df5955908d77d03
robot144 <robot144@protonmail.com>
2023-11-13 21:18:36 +0100

    Add updated data for ZUNORMM that includes z-velocity and eddy-viscosity

--- case_zunormm/data/download_data.sh
+++ case_zunormm/data/download_data.sh
@@ -3,6 +3,6 @@
 # 
 #curl -o zunormm.tgz  https://nx7384.your-storageshare.de/s/BbWGcMxHr4G3TpQ/download
 # new datafile with waterdepth included
-curl -o zunormm.tgz  https://nx7384.your-storageshare.de/s/5Br4YNqjCiZK3ms/download
+curl -o zunormm.tgz  https://nx7384.your-storageshare.de/s/BrmdQjDXnao7FYS/download
 
 tar -xzf zunormm.tgz
